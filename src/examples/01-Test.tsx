20e2edd25b25d7b24da4c4ce766a259217667a79
Jessica Yeh <jessicayeh@gmail.com>
2020-06-27 10:28:03 -0500

    add storybook and material ui

---
+++ src/examples/01-Test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const Test: React.FC = () => {
+    return (<Button>test</Button>)
+}
+
+export default Test;
---
+++ src/stories/1-Button.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Test from '../examples/01-Test';
+
+export default {
+  title: 'Button',
+  component: Test,
+};
+
+export const Text = () => <Test />;
